292fc3c4c7bac653bc1ea625963899a2b6725b79
Miguel Sosa Manríquez <miguelsosam94@gmail.com>
2021-11-15 23:19:54 -0600

    All rules has been implemented. No testing yet

--- src/main/java/mx/msosa/controller/CstController.java
+++ src/main/java/mx/msosa/controller/CstController.java
@@ -1,13 +1,27 @@
 package mx.msosa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import mx.msosa.service.CstService;
 
 @RestController
 public class CstController {
 	
-	public String pigLatin(String englishWord) {
+	@Autowired
+	CstService service;
+	
+	
+	/* Pig Latin Controller
+	 * Controller is the first layer of a microservice, this class will interact with frontend side.
+	 */
+	@GetMapping(value="translate/{word}", produces=MediaType.TEXT_PLAIN_VALUE)
+	public String pigLatin(@PathVariable("word") String englishWord) {
 		
-		return "";
+		return service.translateToPig(englishWord);
 		
 	}
 
